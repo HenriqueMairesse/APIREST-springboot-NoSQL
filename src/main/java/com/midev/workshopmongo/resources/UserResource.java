bb35c620e26a138c00c840c2d6bb724da6ea5787
MImortal <hvmkiki@gmail.com>
2026-02-25 11:38:54 -0300

    Obtendo um usuário por id

--- src/main/java/com/midev/workshopmongo/resources/UserResource.java
+++ src/main/java/com/midev/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import com.midev.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,12 @@ public class UserResource {
         List<User> list = service.findAll();
         List<UserDto> listDto = list.stream().map(x -> new UserDto(x)).toList();
         return ResponseEntity.ok().body(listDto);
+    }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDto> findById(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDto(obj));
     }
 
 }
